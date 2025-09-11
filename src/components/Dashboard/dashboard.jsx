73800870e0a6d3767944111999824fe95b113df9
KunalMohnani <nannumohnani384@gmail.com>
2025-09-11 11:24:09 +0530

    update

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -18,9 +18,29 @@ const dashboard = () => {
         </div>
         <div className={styles.dashboard0}>
           <div className={styles.dashboard1}>
-            <div className={` ${styles.bankacc} ${styles.overall}`}>hello</div>
-            <div className={` ${styles.loans} ${styles.overall}`}></div>
-            <div className={` ${styles.insurance} ${styles.overall}`}></div>
+            <div className={` ${styles.bankacc} ${styles.overall}`}>
+              <h5>Bank Accounts</h5>
+              <span className={styles.bank}>1. HDFC Bank:   <span>******3457</span></span><br />
+              <span className={styles.bank}>2. SBI Bank:   <span>******2463</span></span>
+            </div>
+            <div className={` ${styles.loans} ${styles.overall}`}>
+              <h5>Loans and EMIs</h5>
+              <div>
+                <span className={styles.bank}>1. HDFC Home Loan</span><br />
+              </div>
+              <div>
+                <span className={styles.bank}>2. Car EMIs  </span>
+              </div>
+            </div>
+            <div className={` ${styles.insurance} ${styles.overall}`}>
+              <h5>Insurance</h5>
+              <div>
+                <span className={styles.bank}>1. Life Insurance : <span>7th Oct</span></span><br />
+              </div>
+              <div>
+                <span className={styles.bank}>2. Vehicle Insurance: 30th Sep</span>
+              </div>
+            </div>
           </div>
           <div className={styles.dashboard1}>
             <div className={` ${styles.income} ${styles.overall}`}>
@@ -36,10 +56,19 @@ const dashboard = () => {
               <Individual />
             </div>
             <div className={` ${styles.mutual} ${styles.overall}`}>
-              
+              <h5>Mutual Funds</h5>
+              <div>
+                <span className={styles.bank}>1. Equity Fund  : <span>12%</span></span><br />
+                <span className={styles.bank}>2. Gapont Fund: <span>3%</span></span><br />
+                
+              </div>
             </div>
             <div className={` ${styles.equity} ${styles.overall}`}>
-              
+              <h5>Equity Shares</h5>
+              <div>
+                <span className={styles.bank}>1.Tata Motors : <span>₹2,34,000</span></span><br />
+                <span className={styles.bank}>2.Reliance : <span>₹1,20,000</span></span><br />
+              </div>
             </div>
            
           </div>
