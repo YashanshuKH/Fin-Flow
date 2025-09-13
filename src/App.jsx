0421544906e34abc352512a2ab56551cdab867a7
YashanshuKH <yashanshukh337@gmail.com>
2025-09-13 22:58:17 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
 
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      
       <Route path="/insurance" element={<Insurance />} />
 
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from "./Sidebar.module.css";
 import { HashLink } from "react-router-hash-link";
+import { Link } from "react-router-dom";
+
 import {
   FaHome,
   FaExchangeAlt,
@@ -8,52 +10,69 @@ import {
   FaMoneyCheck,
   FaFileInvoice,
   FaShieldAlt,
-  FaChartBar
+  FaChartBar,
 } from "react-icons/fa";
 
-const Sidebar=()=> {
+const Sidebar = () => {
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.logo}>Dashboard</div>
-
       <ul className={styles.sidebar_menu}>
-        <li className="active"><FaHome className="icon" /><HashLink className={styles.sidebar_link} smooth to="/dashboard">Dashboard</HashLink></li>
-        <li><FaExchangeAlt className="icon" /><span>Transactions</span></li>
-        <li><FaChartPie className="icon" /><span>Investments</span></li>
-        
-          <div className={styles.section}>
-        <h4>Loans</h4>
-        <ul className={styles.sidebar_menu}>
-          <li><FaMoneyCheck className="icon" /><span>Home Loan</span></li>
-          <li><FaMoneyCheck className="icon" /><span>Car Loan</span></li>
-          <li><FaMoneyCheck className="icon" /><span>Personal Loan</span></li>
-        </ul>
-      </div>
-
-      <div className={styles.section}>
-        <h4>Insurance</h4>
-        <ul className={styles.sidebar_menu}>
-          <li><FaFileInvoice className="icon" /><HashLink className={styles.sidebar_link} smooth to="/insurance#life">Life</HashLink></li>
-          <li><FaFileInvoice className="icon" /><HashLink className={styles.sidebar_link} smooth to="/insurance#medical">Medical</HashLink></li>
-          <li><FaFileInvoice className="icon" /><HashLink className={styles.sidebar_link} smooth to="/insurance#vehicle">Vehicle</HashLink></li>
-        </ul>
-      </div>
-
-      <div className={styles.section}>
-        <h4>Mutual Funds</h4>
-        <ul className={styles.sidebar_menu}>
-          <li><FaShieldAlt className="icon" /><span>Enquiry Fund</span></li>
-        </ul>
-      </div>
-
-      <hr />
-
-        <li><FaChartBar className="icon" /><span>Reports</span></li>
-        <li><FaCog className="icon" /><span>Settings</span></li>
+        <li className="active">
+          <FaHome className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/dashboard">
+            Dashboard
+          </HashLink>
+        </li>
+        <li className="active">
+          <FaExchangeAlt className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/transaction">
+            Transaction
+          </HashLink>
+        </li>
+        <li>
+          <FaChartPie className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/investment">
+            Investment
+          </HashLink>
+        </li>
+        <li>
+          <FaMoneyCheck className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/loans">
+            Loans
+          </HashLink>
+        </li>
+        <li>
+          <FaFileInvoice className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/insurance">
+            Insurance
+          </HashLink>
+        </li>
+        <li>
+          <FaShieldAlt className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/mutualfunds">
+            Mutual Funds
+          </HashLink>
+        </li>
+        <li>
+          <FaChartPie className="icon" />
+          <HashLink className={styles.sidebar_link} smooth to="/equityshares">
+            Equity Shares
+          </HashLink>
+        </li>
+    <hr />
+        <div>
+        <li>
+          <FaChartBar className="icon" />
+          <span>Reports</span>
+        </li>
+        <li>
+          <FaCog className="icon" />
+          <span>Settings</span>
+        </li>
+        </div>
       </ul>
-
     </aside>
   );
-}
+};
 
 export default Sidebar;
