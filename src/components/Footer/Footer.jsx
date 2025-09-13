4e9d7e5be79919a6feb2ab2c0ca6ba3cfa77e7c0
YashanshuKH <yashanshukh337@gmail.com>
2025-09-13 18:22:46 +0530

    Improved css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,43 @@
-import { AiOutlineFacebook } from "react-icons/ai";
-import { FaInstagram } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top w-100">
-  
-  <div className="d-flex align-items-center">
-    <Link to="/" className="me-2 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap">
-      <svg className="bi" width="30" height="24" aria-hidden="true">
-        <use xlink:to="#bootstrap"></use>
-      </svg>
-    </Link>
-    <span className="text-body-secondary">© 2025 Company, Inc</span>
-  </div>
- 
-     <div className="d-flex align-items-center">
-    <div className="me-4 text-end">
-          <p className="mb-0 text-body-secondary">
-            Email: <a className={styles.link} href="mailto:finflow@gmail.com">finflow@gmail.com</a>
-          </p>
-          <p className="mb-0 text-body-secondary">
-            Contact: <a className={styles.link} href="tel"> 9865321475</a>
-          </p>
-        </div>
-
+    <footer
+      className={`d-flex flex-wrap justify-content-between align-items-center py-3 border-top w-100 ${styles.footer}`}
+    >
+      {/* Left side */}
+      <div className="d-flex align-items-center">
+        <Link
+          to="/"
+          className="me-2 text-decoration-none lh-1"
+          aria-label="FinFlow"
+        >
+          <svg className="bi" width="30" height="24" aria-hidden="true">
+            <use xlinkHref="#bootstrap"></use>
+          </svg>
+        </Link>
+        <span>© 2025 Company, Inc</span>
       </div>
 
-
-</footer>
-
+      {/* Middle */}
+      <div className="d-flex align-items-center">
+        <div className="me-4 text-end">
+          <p className="mb-0">
+            Email:{" "}
+            <a className={styles.link} href="mailto:finflow@gmail.com">
+              finflow@gmail.com
+            </a>
+          </p>
+          <p className="mb-0">
+            Contact:{" "}
+            <a className={styles.link} href="tel:+919865321475">
+              +91 98653 21475
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
